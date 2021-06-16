946faf1e325872e3a8dc8f97f78b952bf54566c4
root <root@CTD-Quiz-Website-NTH.2zdvgh11mavutkaqo0u1shq1zb.bx.internal.cloudapp.net>
2021-06-16 03:08:15 +0000

    NTH Deployment working

--- NTH/frontend/src/axios.js
+++ NTH/frontend/src/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-const BASE_URL = "http://127.0.0.1:8000";
+
+const BASE_URL = window.location.origin + '/api';
 
 const axiosInstance = axios.create({
     baseURL:BASE_URL,
-    timeout:5000,
+    timeout:50000,
     headers:{
         Authorization:localStorage.getItem('token')
         ? "Token "+localStorage.getItem('token') 
