b5e87279fd76a9e94f1a15f1f90e6ca1a7421508
Sanket Kulkarni <sakulkarni0801@gmail.com>
2021-06-25 19:56:15 +0530

    Web team fixed

--- Website/frontend/src/pages/WebTeam/WebTeam.js
+++ Website/frontend/src/pages/WebTeam/WebTeam.js
@@ -24,33 +24,21 @@ const WebTeam = () => {
       <br />
       <Container fluid>
         <Row>
-          <Col
-            className="d-flex justify-content-end align-items-end"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center1" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/fatema-katawala-2b99451a1/"
               name="Fatema Katawala"
               image={Fateema}
             />
           </Col>
-          <Col
-            className="d-flex justify-content-center align-items-center"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center2" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/rakmo33/"
               name="Omkar Dabir"
               image={Omkar}
             />
           </Col>
-          <Col
-            className="d-flex justify-content-start align-items-start"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center3" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/shreya-deshpande-420297191/"
               name="Shreya Deshpande"
@@ -59,34 +47,21 @@ const WebTeam = () => {
           </Col>
         </Row>
         <Row>
-          <Col
-            className="d-flex justify-content-end align-items-end"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center1" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/atharva-naphade-540236148/"
               name="Atharva Naphade"
               image={Atharva}
             />
           </Col>
-          <Col
-            className="d-flex justify-content-center align-items-center"
-            lg={4}
-            md={6}
-            sm={12}
-          >
+          <Col className="center2" lg={4} md={6} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/karan-lakhwani"
               name="Karan Lakhwani"
               image={Karan}
             />
           </Col>
-          <Col
-            className="d-flex justify-content-start align-items-start"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center3" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/neil-deshpande-908aa118b/"
               name="Neil Deshpande"
@@ -98,22 +73,14 @@ const WebTeam = () => {
       <Container>
         <Row>
           <Col lg={2} sm={12}></Col>
-          <Col
-            className="d-flex justify-content-start align-items-start"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center3" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/pranav-mohril/"
               name="Pranav Mohril"
               image={Pranav}
             />
           </Col>
-          <Col
-            className="d-flex justify-content-end align-items-end"
-            lg={4}
-            sm={12}
-          >
+          <Col className="center1" lg={4} sm={12}>
             <TeamMemberCard
               linkedin="https://www.linkedin.com/in/sanket-a-kulkarni/"
               name="Sanket Kulkarni"
