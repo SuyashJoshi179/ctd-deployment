1218b20bf08451d6884f0dd78bde48086fb61090
Suyash More <suyashmore789@gmail.com>
2021-07-21 03:07:47 +0530

    browsable api view restricted

--- NTH/backend/NTH/NTH/settings.py
+++ NTH/backend/NTH/NTH/settings.py
@@ -61,7 +61,12 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
 
 ROOT_URLCONF = 'NTH.urls'
