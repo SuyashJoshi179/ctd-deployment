6641342ef63437baf483a2c5b3d5770ab3ca046d
Karan Lakhwani <karan.lakhwani0910@gmail.com>
2021-07-24 13:02:14 +0530

    final changes

--- NTH/frontend/src/Login/Instructions.js
+++ NTH/frontend/src/Login/Instructions.js
@@ -3,29 +3,39 @@ const Instructions = () => {
     <div className="instructions">
       <h4>&lt;instructions&gt;</h4>
       <div className="instructions-content">
-        <h4>
-          <ul>
-            <li>
-              Do not prompt or reveal the answers/hints anywhere else you will
-              be disqualified.
-            </li>
-            <li>
-              We reserve the right to ban any user for the use of any unfair
-              means.
-            </li>
-            <li>
-              The decision regarding winners by the NTH team is final and
-              binding.
-            </li>
-          </ul>
-        </h4>
+        <ul>
+          <li>
+            After logging in, there will be a question displayed which includes
+            pictures and hints.
+          </li>
+          <li>
+            As you have logged in, your URL will be
+            https://nth.pictieee.in/question To submit your answer, you have to
+            put a '/' in the URL followed by the answer.
+          </li>
+          <li>
+            If your answer is correct, you will go to the next level, otherwise,
+            you will be greeted with the same question with same level.
+          </li>
+          <li>
+            The one who solves as many questions and reaches the top level is
+            displayed on the leaderboard.
+          </li>
+          <li>
+            If your answer is correct, you will go to the next level, otherwise,
+            you will stay on the same level with the same question.
+          </li>
+          <li>
+            After the 24-hour hunt, the one on the top of the leaderboard wins!
+          </li>
+          <li>
+            The winner will be declared once and there will be no change in our
+            decision. Furthermore, users should play with one account only.
+          </li>
+        </ul>
       </div>
       <h4>&lt;/instructions&gt;</h4>
-      <div className='answer-1'>
-        <h2>
-        We have nothing to give you for the third question.
-        </h2>
-      </div>
+      
     </div>
   );
 };
--- NTH/frontend/src/Login/Login.js
+++ NTH/frontend/src/Login/Login.js
@@ -1,16 +1,18 @@
 import { Col, Row, Container } from "react-bootstrap";
 import "./Login.css";
-import Instructions from './Instructions';
-import LoginForm from './LoginForm';
+import Instructions from "./Instructions";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
     <div>
       <Container fluid>
         <Row>
-          
           <Col lg={6} sm={12}>
             <LoginForm></LoginForm>
+            <div className="answer-1">
+        <h6>We have nothing to give you for the fifth question.</h6>
+      </div>
           </Col>
           <Col lg={6} sm={12}>
             <Instructions></Instructions>
